3560955ca99eae25643c76aad4d21fb402b5f26b
htran <htranquang6@gmail.com>
2026-03-24 12:40:29 +0700

    yay

---
+++ First_Dialog.java
@@ -0,0 +1,8 @@
+package Lab01;
+import javax.swing.JOptionPane;
+public class First_Dialog {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello world! How are you?");
+        System.exit(0);
+    }
+}
