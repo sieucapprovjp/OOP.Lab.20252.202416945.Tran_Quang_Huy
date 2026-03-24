6cfe8b532ada08b829b75c878da70e6e955bf8bf
htran <htranquang6@gmail.com>
2026-03-24 13:57:52 +0700

    Fix: converted Lab01 from submodule to a normal directory

---
+++ src/Lab01/Hello_World.java
@@ -0,0 +1,6 @@
+public class Hello_World {
+    public static void main(String args[]){
+        System.out.println("Xin chao \n cac ban!");
+        System.out.println("Hello \t world!");
+    }
+}
