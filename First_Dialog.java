0a5bdad3eb99170b1dd39d3288ab211e48beb7f7
htran <htranquang6@gmail.com>
2026-03-24 13:40:42 +0700

    Move lab folders into src and clean up root

--- First_Dialog.java
+++
@@ -1,8 +0,0 @@
-package Lab01;
-import javax.swing.JOptionPane;
-public class First_Dialog {
-    public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Hello world! How are you?");
-        System.exit(0);
-    }
-}
--- src/Lab01/Hello_World.java
+++
@@ -1,6 +0,0 @@
-public class Hello_World {
-    public static void main(String args[]){
-        System.out.println("Xin chao \n cac ban!");
-        System.out.println("Hello \t world!");
-    }
-}
