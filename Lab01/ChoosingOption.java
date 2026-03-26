07d790b86634ceebc63ce920110ff9a84f402307
htran <htranquang6@gmail.com>
2026-03-26 21:22:11 +0700

    last commit (hope so)

---
+++ Lab01/ChoosingOption.java
@@ -0,0 +1,12 @@
+// Ex 6.1
+// Write, compile and run the ChoosingOption program:
+package Lab01;
+import javax.swing.JOptionPane;
+
+public class ChoosingOption {
+    public static void main(String[] args) {
+        int option = JOptionPane.showConfirmDialog(null,"Do you want to change to the first class tickets?");
+        JOptionPane.showMessageDialog(null,"You have chosen: " + (option==JOptionPane.YES_OPTION?"YES":"NO"));
+        System.exit(0);
+    }
+}
