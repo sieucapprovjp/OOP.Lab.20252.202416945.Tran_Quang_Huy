9085ca16d2f4b7cf9d092c57eeaad9a73b8ec2b6
htran <htranquang6@gmail.com>
2026-04-05 16:31:50 +0700

    yay commit

--- Lab02/AIMS.java
+++ Lab02/AIMS.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
-
 public class AIMS {
+
+    public static void main(String[] args) {
+
+        // Section 12: Create a new cart
+        Cart anOrder = new Cart();
+
+        // Create DVD objects and add to cart
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
+                "Animation", "Roger Allers", 87, 19.95f);
+        anOrder.addDigitalVideoDisc(dvd1);
+
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars",
+                "Science Fiction", "George Lucas", 87, 24.95f);
+        anOrder.addDigitalVideoDisc(dvd2);
+
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin",
+                "Animation", 18.99f);
+        anOrder.addDigitalVideoDisc(dvd3);
+
+        // Display cart and total cost
+        anOrder.displayCart();
+
+        // Section 13: Remove a DVD and display again
+        System.out.println("\n--- Removing Star Wars ---");
+        anOrder.removeDigitalVideoDisc(dvd2);
+        anOrder.displayCart();
+
+        // Section 14.2: Test overloading - add two DVDs at once
+        System.out.println("\n--- Adding two DVDs at once ---");
+        DigitalVideoDisc dvd4 = new DigitalVideoDisc("Frozen", "Animation", 24.99f);
+        DigitalVideoDisc dvd5 = new DigitalVideoDisc("Moana", "Animation", 22.99f);
+        anOrder.addDigitalVideoDisc(dvd4, dvd5);
+        anOrder.displayCart();
+
+        // Section 16: Test classifier member
+        System.out.println("\nTotal DVDs created: " + DigitalVideoDisc.getNbDigitalVideoDiscs());
+    }
 }
--- Lab02/Testingparameter.java
+++ Lab02/Testingparameter.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
-
 public class Testingparameter {
+
+    public static void main(String[] args) {
+
+        // Section 15: Test pass by value
+        DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
+        DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
+
+        // swap() - references are passed by value, so swap has NO effect outside
+        swap(jungleDVD, cinderellaDVD);
+        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+        System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
+
+        // changeTitle() - the object itself is mutated via the reference copy, so it DOES change
+        changeTitle(jungleDVD, cinderellaDVD.getTitle());
+        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+    }
+
+    // Swapping references has no effect outside (pass by value)
+    public static void swap(Object o1, Object o2) {
+        Object tmp = o1;
+        o1 = o2;
+        o2 = tmp;
+    }
+
+    // Mutating the object via its reference copy DOES affect the original
+    public static void changeTitle(DigitalVideoDisc dvd, String title) {
+        String oldTitle = dvd.getTitle();
+        dvd.setTitle(title);
+        dvd = new DigitalVideoDisc(oldTitle);
+    }
 }
